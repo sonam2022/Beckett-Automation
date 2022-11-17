c0a303830645cfdc94b029485b465e0080becad8
Sonam2022 <sonam2686@gmail.com>
2022-11-17 21:46:20 +0530

    1711

---
+++ cypress/TestArcanetinmen/DSWebshopprodlistpagtion.specs.js
@@ -0,0 +1,53 @@
+import { recurse } from 'cypress-recurse'
+
+describe.skip('Product Listing Pagination',()=>{
+
+beforeEach(()=>{
+
+cy.fixture("logindetails.json").then(function(data){
+
+    globalThis.logindata = data;
+
+})
+
+it('Product Listing Pagination',()=>{
+
+    cy.visit('/webshop' ,'{timeout:50000000}')
+
+    cy.get('#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll').click()
+
+    cy.get('.ti-user').trigger('mouseover')
+
+cy.contains('Sign in/Register').click({ force: true })
+
+cy.get('#Email').type(logindata.email)
+
+cy.get('#Password').type(logindata.password)
+
+cy.get('.btn').eq(0).click()
+
+cy.get('.leo-top-menu > .nav > .active > .nav-link > .menu-title').click({force:true}).wait(20000)
+
+const product =':nth-child(1) > .product-miniature > .thumbnail-container > .product-image > .functional-buttons > .pro-info-onhover > a'
+
+recurse(
+
+    ()=> cy.get('.next'),($button)=>$button.hasClass('disabled')=='disabled',
+
+   {
+
+post(){
+
+           cy.log("start");
+           cy.get('.next ').should('be.visible').click({force:true});
+   cy.log("end");
+   cy.wait(20000)
+
+}
+
+})
+
+
+})
+})
+})
